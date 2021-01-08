b06628c6d4003479dbc691ca31b64df98d98a253
Ray-Garraty <vl.besed@gmail.com>
2021-01-08 15:35:37 +0300

    as a quick fix connected bootstrap css styles directly via html file

--- src/bootstrap.init.js
+++ src/bootstrap.init.js
@@ -1,5 +1,6 @@
 import 'bootstrap/js/dist/modal';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
 import init from './app.init.js';
 
 export default () => init();
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -17,6 +17,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'template.html',
+      inject: false,
     }),
   ],
 };
